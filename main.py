98432bc592abd12ea2823c1dbb3ee183ac351537
Kevin <kevinarmandzakka@gmail.com>
2020-01-03 23:17:11 +0200

    bugfix

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
   # frustums in the dataset
   # ======================================================================================================== #
   print("Estimating voxel volume bounds...")
-  n_imgs = 1000
+  n_imgs = 500
   cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
   vol_bnds = np.zeros((3,2))
   for i in range(n_imgs):
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))
 
     # Integrate observation into voxel volume (assume color aligned with depth)
-    tsdf_vol.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
+    tsdf_vol.integrate_cpp(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
 
   fps = n_imgs / (time.time() - t0_elapse)
   print("Average FPS: {:.2f}".format(fps))
